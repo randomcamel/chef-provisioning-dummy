6206648ec0adcb8663d1b883833aa541a6856362
Chris Doherty <cdoherty@chef.io>
2015-07-23 11:35:52 -0700

    cleanup.

--- spec/dummy_spec.rb
+++ spec/dummy_spec.rb
@@ -1,2 +1,16 @@
-require 'spec_helper'
+describe "Chef::Provisioning::Dummy" do
+  extend DummySupport
+  include DummyConfig
 
+  when_the_chef_12_server "exists", server_scope: :context, port: 8900..9000 do
+    with_dummy "integration tests" do
+      context "machine resource" do
+        it "runs :create by default" do
+          expect_recipe {
+            machine "fake-machine"
+          }
+        end
+      end
+    end
+  end
+end
