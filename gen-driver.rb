0751fc339c3070b06eac6b6ee9a01bbed7f69a0e
Chris Doherty <cdoherty@chef.io>
2015-07-22 19:31:57 -0700

    probably matches my understanding?

--- gen-driver.rb
+++ gen-driver.rb
@@ -20,7 +20,7 @@
 
 underscore_name = ARGV[1].downcase
 underscore_driver = "#{underscore_name}_driver"
-driver_dir = "chef-provisioning-#{underscore_name.gsub('-', '-')}"
+driver_dir = "#{ENV['PWD']}/chef-provisioning-#{underscore_name.gsub('-', '-')}"
 
 camel_name = underscore_name.split('_').collect(&:capitalize).join
 
@@ -45,9 +45,57 @@ file prefix("#{underscore_driver}.rb") do
   EOS
 end
 
+file prefix("#{underscore_driver}/version.rb") do
+  content <<-EOS
+class Chef
+module Provisioning
+module #{camel_name}Driver
+  VERSION = '0.1'
+end
+end
+end
+EOS
+  end
 
-execute "rspec --init && echo '-fd' >> .rspec"
+file prefix("#{underscore_driver}/driver.rb")
 
-%w{spec support}.each do |f|
-  file "spec/#{underscore_name}_#{f}.rb"
+execute "rspec --init && echo '-fd' >> .rspec" do
+  cwd driver_dir
+  not_if { File.exist?("spec") }
+end
+
+file "spec/#{underscore_name}_spec.rb"
+
+file "spec/#{underscore_name}_support.rb" do
+  content <<-EOS
+module #{camel_name}Support
+  require 'cheffish/rspec/chef_run_support'
+  def self.extended(other)
+    other.extend Cheffish::RSpec::ChefRunSupport
+  end
+
+  def with_dummy(description, *tags, &block)
+    context_block = proc do
+      #{underscore_name}_driver = Chef::Provisioning.driver_for_url("#{underscore_name}")
+
+      @@driver = #{underscore_name}_driver
+      def self.driver
+        @@driver
+      end
+
+      module_eval(&block)
+    end
+
+    when_the_repository "exists and \#{description}", *tags, &context_block
+  end
+end
+
+module #{camel_name}Config
+  def chef_config
+    @chef_config ||= {
+      driver:       Chef::Provisioning.driver_for_url("#{underscore_name}"),
+    }
+  end
+end
+EOS
 end
